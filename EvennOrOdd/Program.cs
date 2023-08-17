7abc3a70c623411214a74d786b65d6639801fa3d
Megan Moore <meganmoore.dev@gmail.com>
2023-08-17 14:45:27 -0400

    Add project files.

---
+++ EvennOrOdd/Program.cs
@@ -0,0 +1,33 @@
+﻿var quit = false;
+while (!quit)
+{
+
+
+
+    Console.Write("Enter an integer: ");
+    var answer = Console.ReadLine();
+    Console.WriteLine($"Answer is {answer}");
+    var number = Convert.ToInt32(answer);
+
+
+    string evenOrOdd;
+    if (number % 2 == 0)
+    {
+        evenOrOdd = "Even";
+    }
+    else
+    {
+        evenOrOdd = "Odd";
+    }
+
+    Console.WriteLine($"The number is {number} is {evenOrOdd}");
+
+    Console.WriteLine("Do you want to quit? (Y/N) :");
+    var quitAnswer = Console.ReadLine();
+    if (quitAnswer == "Y" || quitAnswer == "y")
+    {
+        quit = true;
+    }
+}
+
+Console.WriteLine("Done...");
